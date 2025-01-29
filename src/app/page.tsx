ee74b3613de423d42a006ac5b4c83fda5226754e
piyushmakad <piyush.makad16@gmail.com>
2025-01-29 18:24:29 +0530

    Update: Home page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
-import { Button } from "@/components/ui/button"
+"use client"; 
 
-export default async function Home() {
-  return <Button>Click </Button>
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        router.replace("/dashboard"); 
+      } else {
+        router.replace("/sign-in"); 
+      }
+    }
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Button disabled>Redirecting...</Button>
+    </div>
+  );
 }
