e164ece61903e683a647e12b53dcb08b3f3ce8b9
piyushmakad <piyush.makad16@gmail.com>
2025-01-29 18:34:53 +0530

    Update: For Vercel

--- src/app/api/process-meeting/route.ts
+++ src/app/api/process-meeting/route.ts
@@ -9,7 +9,7 @@ const bodyParser = z.object({
   meetingId: z.string(),
 });
 
-export const maxDuration = 300; //5 minutes
+export const maxDuration = 60; //1 minutes
 
 export async function POST(req: NextRequest) {
   const { userId } = await auth();
